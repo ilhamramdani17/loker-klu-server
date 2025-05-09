bc03a150955b83bff4c8126b2a8cf99340d18b5b
ilhamramdani17 <127287773+ilhamramdani17@users.noreply.github.com>
2025-05-09 22:11:29 +0800

    Update jobsRouter.js

--- routes/jobsRouter.js
+++ routes/jobsRouter.js
@@ -2,13 +2,15 @@ import express from "express";
 import { 
     getJobsData,
     getjobsByQuery,
+    getCurrentJobs,
     getJobsDataBySearch 
 } from "../controllers/jobsController.js";
 
 const jobsRouter = express.Router()
 
+jobsRouter.get('/api/jobs',getJobsData);
 jobsRouter.get('/api/queries',getjobsByQuery);
-jobsRouter.get('/api/jobs/:location',getJobsData);
 jobsRouter.get('/api/jobs/:keyword',getJobsDataBySearch);
+jobsRouter.get('/api/current/jobs/:keyword',getCurrentJobs);
 
 export default jobsRouter;
